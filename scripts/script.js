4b169de295a71ff35f4c007eefa5ea4fb4c747da
Len Tong <Tong.Len004@gmail.com>
2025-11-07 17:52:50 -0800

    Fixed Navigation and added carousal

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,9 @@
-function menu() {
-    document
-}
+let hamenuButton = document.getElementById("nav_button")
+
+hamenuButton.addEventListener("click",() =>{
+    let navMenu = document.getElementById("nav-toggle")
+
+    navMenu.classList.toggle("show-menu")
+
+    hamenuButton.classList.toggle("change")
+})
