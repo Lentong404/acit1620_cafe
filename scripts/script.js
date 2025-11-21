ecccfd394cfbb3137c2bacf2fde59dbcd6c14206
dwhiteBCIT <dwhite170@my.bcit.ca>
2025-11-20 19:47:26 -0800

    fixed some of the images and added prices

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,9 @@
-console.log("Some code");
+let hamenuButton = document.getElementById("nav_button")
+
+hamenuButton.addEventListener("click",() =>{
+    let navMenu = document.getElementById("nav-toggle")
+
+    navMenu.classList.toggle("show-menu")
+
+    hamenuButton.classList.toggle("change")
+})
