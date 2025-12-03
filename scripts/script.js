a089a84a117cf76460e3c2ddc071ca3eb2540eea
Len Tong <Tong.Len004@gmail.com>
2025-12-03 14:20:40 -0800

    Finished contact page

--- scripts/script.js
+++ scripts/script.js
@@ -6,19 +6,29 @@ hamenuButton.addEventListener("click",() =>{
     hamenuButton.classList.toggle("change")
 })
 
-let drinkButton = document.getElementById("drink_button")
-let foodButton = document.getElementById("food_button")
+// let drinkButton = document.getElementById("drink_button")
+// let foodButton = document.getElementById("food_button")
 
-drinkButton.addEventListener("click", () =>{
-    let showDrinks = document.getElementById("show_drinks")
-    showDrinks.classList.toggle("show-drinks")
+// drinkButton.addEventListener("click", () =>{
+//     let showDrinks = document.getElementById("show_drinks")
+//     showDrinks.classList.toggle("show-drinks")
     
+// })
 
+// foodButton.addEventListener("click", () =>{
+//     let showFood = document.getElementById("show_food")
+//     showFood.classList.toggle("show-food")
 
-})
+// })
 
-foodButton.addEventListener("click", () =>{
-    let showFood = document.getElementById("show_food")
-    showFood.classList.toggle("show-food")
+    document.getElementById("toggleButton").addEventListener("click", toggleDisplay);
 
-})
+        function toggleDisplay() {
+      var element = document.getElementById("myDiv"); // Get the element by its ID
+
+      if (element.style.display === "none") {
+        element.style.display = "block"; // If display is none, set it to block
+      } else {
+        element.style.display = "none"; // Otherwise, set it to none
+      }
+    }
